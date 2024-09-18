d3ab00d49488c037bc1ed66fcd7f96cbd129b4ba
chribece <cv.becerrap@uea.edu.ec>
2024-09-17 21:34:31 -0500

    Deber semana 14 Cálculo de Descuento en Compras update

--- semana 14/Calc_Desc.py
+++ semana 14/Calc_Desc.py
@@ -12,7 +12,7 @@ descuento1=calcular_descuento(monto1)
 montof=monto1-descuento1
 print(f"Monto total: ${monto1}")
 print(f"Descuento 10%: ${descuento1}")
-print(f"Valor a cancelar: ${montof}")
+print(f"Valor a cancelar: ${montof}\n")
 
 #llamada 2 funcion calcular descuento
 monto2=100
@@ -20,4 +20,14 @@ descuento2=calcular_descuento(monto2)
 montof2=monto2-descuento2
 print(f"Monto total: ${monto2}")
 print(f"Descuento 10%: ${descuento2}")
-print(f"Valor a cancelar: ${montof2}")
+print(f"Valor a cancelar: ${montof2}\n")
+
+#llamada 3 funcion calcular descuento con argumento porcentaje de descuento diferente
+monto3=1500
+porcentaje3=15
+descuento3=calcular_descuento(monto3,porcentaje3)
+montof3=monto3-descuento3
+print(f"Monto total: ${monto3}")
+print(f"Descuento {porcentaje3}%: ${descuento3}")
+print(f"Valor a cancelar: ${montof3}")
+
